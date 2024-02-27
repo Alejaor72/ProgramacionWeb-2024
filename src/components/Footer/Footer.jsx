09bdafd4244e751f558410fbac3dc8d4af591969
Your Name <you@example.com>
2024-02-26 23:57:12 -0500

    files deleted

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import './style.css'
-import React from 'react'
-export function Footer(){
-    return(
-        <footer>
-            <p>Programación Web 2024</p>
-        </footer>
-    )
-}
--- src/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import './style.css'
-import React from 'react'
-export function Header(){
-    return(
-        <header>
-            <h1>Hi!!♫</h1>
-        </header>
-    )
-}
